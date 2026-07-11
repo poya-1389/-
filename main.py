460d43e3955668b6aaec8ce531686474889b347a
poya-1389 <poyaheidary372@gmail.com>
2026-07-11 23:25:39 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import asyncio
 from datetime import datetime
 import pytz
-from pyrogram import Client
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+from telethon.tl.functions.account import UpdateProfileRequest
 import os
 
 # دریافت اطلاعات از متغیرهای محیطی ریلوی
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
-SESSION_STRING = os.environ.get("SESSION_STRING")
+SESSION_STRING = os.environ.get("SESSION_STRING").strip()
 
-app = Client("my_self", api_id=API_ID, api_hash=API_HASH, session_string=SESSION_STRING)
+# راه اندازی کلاینت تلتون با سشن استرینگ شما
+client = TelegramClient(StringSession(SESSION_STRING), API_ID, API_HASH)
 
 # تبدیل اعداد انگلیسی به فونت بولد درخواستی شما
 def to_bold_font(time_str):
@@ -20,20 +23,33 @@ def to_bold_font(time_str):
     return "".join(bold_digits.get(char, char) for char in time_str)
 
 async def main():
-    async with app:
-        print("سلف با موفقیت روشن شد!")
-        last_time = ""
-        
-        while True:
-            try:
-                # تنظیم تایم زون تهران
-                tehran_tz = pytz.timezone('Asia/Tehran')
-                now = datetime.now(tehran_tz)
-                current_time = now.strftime("%H:%M")
+    print("سلف تلتون با موفقیت روشن شد!")
+    last_time = ""
+    
+    while True:
+        try:
+            # تنظیم تایم زون تهران
+            tehran_tz = pytz.timezone('Asia/Tehran')
+            now = datetime.now(tehran_tz)
+            current_time = now.strftime("%H:%M")
+            
+            # فقط اگر زمان تغییر کرده بود اسم رو آپدیت کن
+            if current_time != last_time:
+                bold_time = to_bold_font(current_time)
                 
-                # فقط اگر زمان تغییر کرده بود اسم رو آپدیت کن
-                if current_time != last_time:
-                    bold_time = to_bold_font(current_time)
+                # آپدیت فامیلی با ساعت بولد شده
+                await client(UpdateProfileRequest(last_name=bold_time))
+                last_time = current_time
+                print(f"ساعت به روز رسانی شد: {bold_time}")
+                
+        except Exception as e:
+            print(f"خطایی رخ داد: {e}")
+            
+        await asyncio.sleep(30) # هر ۳۰ ثانیه چک می‌کنه
+
+if __name__ == "__main__":
+    client.start()
+    client.loop.run_until_complete(main())
                     
                     # دریافت اطلاعات اکانت خودت برای حفظ نام کوچک
                     me = await app.get_me()
